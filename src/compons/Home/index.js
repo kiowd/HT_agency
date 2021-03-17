33a5e18ef3efb5da1842d41824379b5de5499e31
Kyaw <40131560+kiowd@users.noreply.github.com>
2021-03-17 00:44:44 +0000

    changed the font style

--- src/compons/Home/index.js
+++ src/compons/Home/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
 
 const Home = () => {
-  return(
-    <header className='header'>
-<article className='banner'>
-<h1 className='banner_title'>Hi,
-<strong className='banner_title_name'>How're You?</strong>
-  </h1>
-  </article>
-      </header>
-
+  return (
+    <header className="header">
+      <article className="banner">
+        <h1 className="banner_title">
+          <span>Hi,</span>
+          <strong className="banner_title_name">Welcome You</strong>
+        </h1>
+      </article>
+    </header>
   );
-
 };
 
 export default Home;
