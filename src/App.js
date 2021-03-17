95c4bb1293728f9f921ed389592bbd3c961d7c87
Kyaw <40131560+kiowd@users.noreply.github.com>
2021-03-17 00:14:04 +0000

    added header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import React from 'react';
 import PreLoader from "./compons/PreLoader";
 import Navbar from "./compons/Navbar";
+import Home from "./compons/Home";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 //library.add( faCheckSquare, faCoffee)
@@ -12,13 +13,13 @@ export default function App() {
     <div className="container">
       <Router>      
       <Navbar/>
-      {/* <Switch>
+      <Switch>
       <Route path='/' exact component={Home}/>
-      <Route path='/about' component={About}/>
+      {/* <Route path='/about' component={About}/>
       <Route path='/work' component={Work}/>
-      <Route path='/skills' component={Skills}/>
+      <Route path='/skills' component={Skills}/> */}
       </Switch> 
-      */}
+     
       </Router>
       
     </div>
---
+++ src/compons/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return(
+    <header className='header'>
+<article className='banner'>
+<h1 className='banner_title'>Hi,
+<strong className='banner_title_name'>How're You?</strong>
+  </h1>
+  </article>
+      </header>
+
+  );
+
+};
+
+export default Home;
